2906bf61cc6e7482b841f06b3e5990caa989d97f
Roland <roland.burke@gmx.de>
2022-12-27 13:12:42 +0100

    added command line parameters, added configurable paper height, added height examples

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
-# paper-folding
-Run: `.\paper-folding.py` in a terminal
+# PaperFolding
+Run: `.\paper-folding.py <number_of_foldings> <paper_height>` in a terminal
 
-Enter the number of foldings you want to calculate
+<number_of_foldings>: How many times the paper should be folded.
+Optional: <paper_height>: The height of the paper in mm. (Default value: 0.15mm)
 
 Example output:
 ```
 Paper thickness: 0.15mm
-Number of Foldings: 23
+Number of foldings: 23
 
  Height       Unit
  ----------------------
- > 1 000 000  mm
+ 1258291.2    mm
  125829.12    cm
  1258.29      m
+ 59.92        White houses
+ 4.19         Eiffel towers
  1.26         km
+ 0.14         Mount Everests
+ 0.11         Mariana trenches
  0.0          Earth Diameters
- 0.0          Distance to Moon
+ 0.0          Distances to moon
+ 0.0          Distances to sun
  0.0          Lightyears
  0.0          Milky Way Diameters (105700 lightyears)
 ```
--- paper-folding.py
+++ paper-folding.py
@@ -1,36 +1,60 @@
 import sys
 
-PAPER_HEIGHT = 0.00015 # in meter, 0.15mm
+PAPER_HEIGHT_DEFAULT = 0.15 # in mm, 0.15mm
 DECIMAL_PLACES = 2
 
 MM = 0.001
 CM = 0.01
 METER = 1
+WHITE_HOUSE_HEIGHT = 21 # m
+EIFFEL_TOWER_HEIGHT = 300 # m
 KILOMETER = 1000 # m
+MOUNT_EVEREST_HEIGHT = 8848 # m
+MARIANA_TRENCH = 10984 # m
 EARTH_DIAMETER = 12742 # km
 DISTANCE_TO_MOON = 384400 # km
+DISTANCE_TO_SUN = 150000000 # km
 LIGHTYEAR = 1.057 * (10**18) # km
 MILKY_WAY_DIAMETER = 105700 # lightyears
 
 def unit_conversion(height, factor):
     value = round((height / factor), DECIMAL_PLACES)
-    if(value > 1000000):
-        return "> 1 000 000"
+    if(value > 10000000):
+        return "> 10 000 000"
     else:
         return str(value)
 
-print()
-print("Paper thickness: " + unit_conversion(PAPER_HEIGHT, MM) + "mm")
-try:
-    totalFoldings = int(input("Number of Foldings: "))
-except ValueError:
-    print("Please enter an integer!")
-    sys.exit()
-if totalFoldings < 0:
-    print("Please enter a non negative number!")
-    sys.exit()
+paperHeight = PAPER_HEIGHT_DEFAULT
 
-height = PAPER_HEIGHT * (2 ** float(totalFoldings)) # in meter
+if len(sys.argv) == 3:
+	try:
+		totalFoldings = int(sys.argv[1])
+		paperHeight = float(sys.argv[2])
+	except ValueError:
+		print("Please enter a valid integer for total number of foldings and a valid float for the paper height!")
+		sys.exit()
+	if totalFoldings < 0 or paperHeight < 0:
+		print("Please enter a non negative number!")
+		sys.exit()
+elif len(sys.argv) == 2:
+	try:
+		totalFoldings = int(sys.argv[1])
+	except ValueError:
+		print("Please enter a valid integer for total number of foldings!")
+		sys.exit()
+	if totalFoldings < 0:
+		print("Please enter a non negative number!")
+		sys.exit()
+elif len(sys.argv) == 1:
+	print("Usage: " + sys.argv[0] + " <number_of_foldings> optional: <paper_height_in_mm>")
+	sys.exit()
+
+print()
+
+print("Paper thickness: " + str(paperHeight) + "mm")
+print("Number of foldings: " + str(totalFoldings))
+
+height = (paperHeight / 1000.0) * (2 ** float(totalFoldings)) # in meter
 
 print()
 print(f' {"Height": <11}  Unit')
@@ -38,9 +62,14 @@ print(' ----------------------')
 print(f' {unit_conversion(height, MM): <13}' + "mm")
 print(f' {unit_conversion(height, CM): <13}' + "cm")
 print(f' {unit_conversion(height, METER): <13}' + "m")
+print(f' {unit_conversion(height, METER * WHITE_HOUSE_HEIGHT): <13}' + "White houses")
+print(f' {unit_conversion(height, METER * EIFFEL_TOWER_HEIGHT): <13}' + "Eiffel towers")
 print(f' {unit_conversion(height, KILOMETER): <13}' + "km")
+print(f' {unit_conversion(height, METER * MOUNT_EVEREST_HEIGHT): <13}' + "Mount Everests")
+print(f' {unit_conversion(height, METER * MARIANA_TRENCH): <13}' + "Mariana trenches")
 print(f' {unit_conversion(height, KILOMETER * EARTH_DIAMETER): <13}' + "Earth Diameters")
-print(f' {unit_conversion(height, KILOMETER * DISTANCE_TO_MOON): <13}' + "Distance to Moon")
+print(f' {unit_conversion(height, KILOMETER * DISTANCE_TO_MOON): <13}' + "Distances to moon")
+print(f' {unit_conversion(height, KILOMETER * DISTANCE_TO_SUN): <13}' + "Distances to sun")
 print(f' {unit_conversion(height, LIGHTYEAR): <13}' + "Lightyears")
 print(f' {unit_conversion(height, LIGHTYEAR * MILKY_WAY_DIAMETER): <13}' + "Milky Way Diameters (105700 lightyears)")
 print()
